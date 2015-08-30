e87e6e5263446bb0b6479385cc4c65ac9bcdcbeb
Fredrik Noren <fredrik.jw.noren@gmail.com>
2015-08-29 23:54:40 -0400

    Fix grid index

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,8 @@ pub struct Grid {
     pub layout: Layout,
     pub top_left: Vector2<f32>,
     pub size: Vector2<f32>,
-    pub n_vertices: (u32, u32),
+    pub n_vertices_width: u32,
+    pub n_vertices_height: u32,
     pub uv_min: Vector2<f32>,
     pub uv_max: Vector2<f32>,
     pub normal: Vector3<f32>,
@@ -22,7 +23,8 @@ impl Grid {
             layout: Layout::position_texcoord_normal(),
             top_left: Vector2::zero(),
             size: Vector2::one(),
-            n_vertices: (2, 2),
+            n_vertices_width: 2,
+            n_vertices_height: 2,
             uv_min: Vector2::zero(),
             uv_max: Vector2::one(),
             normal: Vector3::new(0.0, 0.0, 1.0),
@@ -35,18 +37,18 @@ impl Grid {
 
 impl From<Grid> for Mesh {
     fn from(grid: Grid) -> Mesh {
-        let n_vertices = (grid.n_vertices.0 * grid.n_vertices.1) as usize;
-        let n_faces = ((grid.n_vertices.0 - 1) * (grid.n_vertices.1 - 1) * 2) as usize;
+        let n_vertices = (grid.n_vertices_width * grid.n_vertices_height) as usize;
+        let n_faces = ((grid.n_vertices_width - 1) * (grid.n_vertices_height - 1) * 2) as usize;
         let mut mesh = Mesh::new(grid.layout.clone(), n_vertices, n_faces);
         let position_attr = mesh.layout.get_attribute(grid.position_attribute.as_str()).cloned();
         let texcoord_attr = mesh.layout.get_attribute(grid.texcoord_attribute.as_str()).cloned();
         let normal_attr = mesh.layout.get_attribute(grid.normal_attribute.as_str()).cloned();
-        for y in 0..grid.n_vertices.1 {
-            for x in 0..grid.n_vertices.0 {
-                let index = (x + y * grid.n_vertices.0) as usize;
+        for y in 0..grid.n_vertices_height {
+            for x in 0..grid.n_vertices_width {
+                let index = (x + y * grid.n_vertices_width) as usize;
                 let p = Vector2::new(
-                    x as f32 / (grid.n_vertices.0 as f32 - 1.0),
-                    y as f32 / (grid.n_vertices.1 as f32 - 1.0));
+                    x as f32 / (grid.n_vertices_width as f32 - 1.0),
+                    y as f32 / (grid.n_vertices_height as f32 - 1.0));
                 if let &Some(ref p_attr) = &position_attr {
                     mesh.write_to_attribute(&p_attr, index, vec![
                         grid.top_left.x + grid.size.x * p.x,
@@ -67,15 +69,16 @@ impl From<Grid> for Mesh {
                         grid.normal.z
                     ]);
                 }
-                if y < grid.n_vertices.1 - 1 && x < grid.n_vertices.0 - 1 {
-                    let index = x + y * (grid.n_vertices.0 - 1);
-                    mesh.element_data[(index * 6) as usize] = index;
-                    mesh.element_data[(index * 6 + 1) as usize] = index + grid.n_vertices.0;
-                    mesh.element_data[(index * 6 + 2) as usize] = index + 1;
+                if y < grid.n_vertices_height - 1 && x < grid.n_vertices_width - 1 {
+                    let ind_index = x + y * (grid.n_vertices_width - 1);
+                    let vert_index = x + y * grid.n_vertices_width;
+                    mesh.element_data[(ind_index * 6 + 0) as usize] = vert_index;
+                    mesh.element_data[(ind_index * 6 + 1) as usize] = vert_index + grid.n_vertices_width;
+                    mesh.element_data[(ind_index * 6 + 2) as usize] = vert_index + 1;
 
-                    mesh.element_data[(index * 6 + 3) as usize] = index + 1;
-                    mesh.element_data[(index * 6 + 4) as usize] = index + grid.n_vertices.0;
-                    mesh.element_data[(index * 6 + 5) as usize] = index + grid.n_vertices.0 + 1;
+                    mesh.element_data[(ind_index * 6 + 3) as usize] = vert_index + 1;
+                    mesh.element_data[(ind_index * 6 + 4) as usize] = vert_index + grid.n_vertices_width;
+                    mesh.element_data[(ind_index * 6 + 5) as usize] = vert_index + grid.n_vertices_width + 1;
                 }
             }
         }
@@ -88,7 +91,8 @@ fn test_grid() {
     let mut grid = Grid::new();
     grid.top_left = Vector2::new(1.0, 2.0);
     grid.size = Vector2::new(6.0, 6.0);
-    grid.n_vertices = (3, 2);
+    grid.n_vertices_width = 3;
+    grid.n_vertices_height = 2;
 
     let mesh: Mesh = grid.into();
     assert_eq!(mesh, Mesh {
@@ -105,7 +109,40 @@ fn test_grid() {
             0, 3, 1,
             1, 3, 4,
             1, 4, 2,
-            2, 4, 5]
+            2, 4, 5
+        ]
+    })
+}
+
+#[test]
+fn test_grid_3_3() {
+    let mut grid = Grid::new();
+    grid.layout = Layout::position();
+    grid.n_vertices_width = 3;
+    grid.n_vertices_height = 3;
+
+    let mesh: Mesh = grid.into();
+    assert_eq!(mesh, Mesh {
+        layout: Layout::position(),
+        vertex_data: vec![
+            0.0, 0.0, 0.0,
+            0.5, 0.0, 0.0,
+            1.0, 0.0, 0.0,
+            0.0, 0.5, 0.0,
+            0.5, 0.5, 0.0,
+            1.0, 0.5, 0.0,
+            0.0, 1.0, 0.0,
+            0.5, 1.0, 0.0,
+            1.0, 1.0, 0.0],
+        element_data: vec![
+            0, 3, 1,
+            1, 3, 4,
+            1, 4, 2,
+            2, 4, 5,
+            3, 6, 4,
+            4, 6, 7,
+            4, 7, 5,
+            5, 7, 8]
     })
 }
 
